14c99a2ca54d645eaaf5afd1279ef0c4b23f1348
Tinnapop.S <tinnapop@getlinks.co>
2019-11-20 16:14:23 +0700

    Setup docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,3 @@
 import { configure } from '@storybook/react'
 
-configure(require.context('../components', true, /\.stories\.js$/), module)
+configure(require.context('../components', true, /\.stories\.(js|mdx)$/), module)
